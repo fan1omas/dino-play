04a82685e3342ff773377443b89fece116f4e383
fantomas <d2nishevsky@gmail.com>
2026-02-23 11:37:42 +0300

    поменял алгоритм проверки

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ from math import sqrt, pow
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1)
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.3)
+
+dots = ((8, 6),   # указательный
+        (12, 10), # средний
+        (16, 14), # безымянный
+        (20, 18)) # мизинец
+
 
 cap = cv2.VideoCapture(0)
 
-def get_coords(landmarks, tip, mcp):
+def get_coords(landmarks, tip, pip):
     tip_coord = (int(landmarks[tip].x * w), int(landmarks[tip].y * h))
-    mcp_coord = (int(landmarks[mcp].x * w), int(landmarks[mcp].y * h))
+    pip_coord = (int(landmarks[pip].x * w), int(landmarks[pip].y * h))
     
-    return tip_coord, mcp_coord
+    return tip_coord, pip_coord
 
 def get_dist(p0, p1):
     x0, y0 = p0
@@ -34,31 +40,30 @@ while cap.isOpened():
         break
     
     frame = cv2.flip(frame, 1)
-    framw = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = hands.process(frame)
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb_frame)
 
     if results.multi_hand_landmarks is not None:
+        fingers_closed = [] 
+
         for hand_landmark, hand_nandedness in zip(results.multi_hand_landmarks,results.multi_handedness):
             landmarks = hand_landmark.landmark
 
-            index_tip_finger, index_mcp_finger = get_coords(landmarks, 8, 5)
-            index_dist = get_dist(index_tip_finger, index_mcp_finger)
+            for dot in dots:
+                tip_y, pip_y = [i[1] for i in get_coords(landmarks, dot[0], dot[1])]
+                fingers_closed.append(tip_y > pip_y)  # y=0 вверху
 
-            middle_tip_finger, middle_mcp_finger = get_coords(landmarks, 12, 9)
-            middle_dist = get_dist(middle_tip_finger, middle_mcp_finger)
+        if all(fingers_closed):
+            print('РУКА СОГНУТА')
+        else:
+            print('РУКА ОТКРЫТА')
 
-            print(index_dist, middle_dist)
-
-            cv2.circle(frame, index_tip_finger, 3, (0, 0, 255), -1)
-            cv2.circle(frame, index_mcp_finger, 3, (0, 0, 255), -1)
-
-            cv2.circle(frame, middle_tip_finger, 3, (0, 0, 255), -1)
-            cv2.circle(frame, middle_mcp_finger, 3, (0, 0, 255), -1)
+        fingers_closed.clear()
+         
     else:
-        print('not found')
+        print('РУКА НЕ ОБНАРУЖЕНА')
     
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
     cv2.imshow('frame', frame)
 
 
